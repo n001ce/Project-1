d06d84438e3747aadfae8951959824f5a094e58a
n001ce <a1ccormier@gmail.com>
2021-07-02 14:54:56 -0400

    added more borders for player to jump around

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ let upKey
 let rightKey
 let downKey
 let leftKey
+let enemy
 
 
 window.onload = function(){
@@ -23,16 +24,16 @@ window.onload = function(){
     //key listeners
     setupInputs()
     //Create Player
-    player = new Player(300,200)
+    player = new Player(1,200)
 
     //Create Borders
-    for(let i = 0; i < 6; i++){
+    borders.push(new Border(1, 300, 400, 100, 100))
+    for(let i = 0; i < 20; i++){
         borders.push(new Border(0+100*i, 620, 100, 100, 1))
     }
     borders.push(new Border(0, 520, 100, 100, 2))
-    for(let i = 0; i < 3; i++){
-        borders.push(new Border(600, 420 + 100 * i, 100, 100, 100))
-    }
+    borders.push(new Border(460, 110, 620, 100, 100))
+    borders.push(new Border(600, 500, 50, 50, 100))
 
     //startGame loop
     gameLoop = setInterval(step, 1000/30)
@@ -52,7 +53,6 @@ function draw(){
     //clear the canvas
     ctx.fillStyle = "white"
     ctx.fillRect(0,0,1280,720)
-
     player.draw()
     borders.forEach((border)=>border.draw())
 }
@@ -61,6 +61,7 @@ function setupInputs(){
     document.addEventListener("keydown", function(e){
         if(e.key ==="w" || e.key === "ArrowUp"){
             upKey = true
+            retur
         }else if(e.key === "a" || e.key==="ArrowLeft"){
             leftKey = true
         }else if(e.key === "d" || e.key === "ArrowRight"){
@@ -68,7 +69,7 @@ function setupInputs(){
         }
     })
     document.addEventListener("keyup", function(e){
-        if(e.key ==="w" || e.key === "ArrowUp"){
+        if(e.key ==="w" || e.key === "ArrowUp" ){
             upKey = false
         }else if(e.key === "a" || e.key==="ArrowLeft"){
             leftKey = false
@@ -91,6 +92,7 @@ function checkIntersection(r1, r2){
 function Player(x,y){
     this.x = x
     this.y = y
+    this.jumping= true,
     this.xspeed = 0 //horizontal speed of player
     this.yspeed = 0 //vertical speed of player
     this.friction = 0.6 //how quickly player slows down
@@ -115,14 +117,14 @@ function Player(x,y){
                 this.yspeed -= 15
             }
             //Gravity
-            this.yspeed += 5
+            this.yspeed += 9
             //vertical movement
 
             //correct the speed
-            if(this.xspeed > this.maxSpeed){this.xspeed = this.maxSpeed}
-            else if(this.xspeed < -this.maxSpeed){this.xspeed = -this.maxSpeed}
-            if(this.yspeed > this.maxSpeed){this.yspeed = this.maxSpeed}
-            else if(this.yspeed < -this.maxSpeed){this.yspeed = -this.maxSpeed}
+            if(this.xspeed > this.maxSpeed) this.xspeed = this.maxSpeed
+            else if(this.xspeed < -this.maxSpeed) this.xspeed = -this.maxSpeed
+            if(this.yspeed > this.maxSpeed) this.yspeed = this.maxSpeed
+            else if(this.yspeed < -this.maxSpeed) this.yspeed = -this.maxSpeed
 
             if(this.xspeed > 0){this.xspeed = Math.floor(this.xspeed)}
             else{this.xspeed = Math.ceil(this.xspeed)}
